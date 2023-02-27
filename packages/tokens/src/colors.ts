3974140438d5404e2d5510bbaa8dd81b1ad27dd2
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-02-27 09:34:52 -0300

    First commit with colors token

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+    white: '#FFF',
+    black: '#000',
+  
+    gray100: '#E1E1E6',
+    gray200: '#A9A9B2',
+    gray400: '#7C7C8A',
+    gray500: '#505059',
+    gray600: '#323238',
+    gray700: '#29292E',
+    gray800: '#202024',
+    gray900: '#121214',
+  
+    iToddy300: '#ffff77',
+    iToddy500: '#f8e44b',
+    iToddy700: '#eeca06',
+    iToddy900: '#d2b202',
+  }
