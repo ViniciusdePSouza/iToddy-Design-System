6ca96a00c81e9242a43b9f3c3f22c40134a154a6
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-02-28 11:41:51 -0300

    Documentation: colors token added

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,30 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
     fontFamily: '$default',
-    backgroundColor: '$iToddy500',
+    backgroundColor: '$iToddy700',
     borderRadius: '$md',
+    border: 0,
+    fontWeight: 'bold',
+    color: "$black",
+
+    variants: {
+        size: {
+            small: {
+                fontSize: 14,
+                padding: '$2 $4',
+            },
+            big: {
+                fontSize: 16,
+                padding: '$3 $6',
+            }
+        }
+    },
+
+    defaultVariants: {
+        size: 'small'
+    }
 })
 
-export function App() {
-    return (
-        <Button>Hello World</Button>
-    )
-}
+export type ButtonProps = ComponentProps<typeof Button>
